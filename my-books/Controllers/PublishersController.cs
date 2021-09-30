19dfbd8d3c6e3a8dfb902b0c4e001e5487463bf2
Ólafur Georg <oligeorg@hotmail.com>
2021-09-30 08:25:25 +0000

    third commit

--- my-books/Controllers/PublishersController.cs
+++ my-books/Controllers/PublishersController.cs
@@ -25,5 +25,19 @@ namespace my_books.Controllers
             _publishersService.AddPublisher(publisher);
             return Ok();
         }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _response = _publishersService.GetPublisherData(id);
+            return Ok(_response);
+        }
+
+        [HttpDelete("delete-publisher-by-id/{id}")]
+        public IActionResult DeletePublisherById(int id)
+        {
+            _publishersService.DeletePublisherById(id);
+            return Ok();
+        }
     }
 }
--- my-books/Data/Services/PublishersService.cs
+++ my-books/Data/Services/PublishersService.cs
@@ -24,5 +24,32 @@ namespace my_books.Data.Services
             _context.Publishers.Add(_publisher);
             _context.SaveChanges();
         }
+
+        public PublisherWithBooksAndAuthorsVM GetPublisherData(int publisherId)
+        {
+            var _publisherData = _context.Publishers.Where(n => n.Id == publisherId)
+                .Select(n => new PublisherWithBooksAndAuthorsVM()
+                {
+                    Name = n.Name,
+                    BookAuthors = n.Books.Select(n => new BookAuthorVM()
+                    {
+                        BookName = n.Title,
+                        BookAuthors = n.Book_Authors.Select(n => n.Author.FullName).ToList()
+                    }).ToList()
+                }).FirstOrDefault();
+
+            return _publisherData;
+        }
+
+        public void DeletePublisherById(int id)
+        {
+            var _publisher = _context.Publishers.FirstOrDefault(n => n.Id == id);
+
+            if (_publisher != null)
+            {
+                _context.Publishers.Remove(_publisher);
+                _context.SaveChanges();
+            }
+        }
     }
 }
